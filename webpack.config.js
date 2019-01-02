68ab273c57aa9a18b3a1dc311d84aeb37054c492
Michael Hadley <michaelwesthadley@gmail.com>
2019-01-02 14:44:27 -0500

    Switch from parcel to webpack for easier control of asset loading

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: "./js/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: "raw-loader"
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: "file-loader"
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: "file-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
+  ]
+};
