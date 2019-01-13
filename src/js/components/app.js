446bfa545f972347ed411da9d0619d75160c2f69
Michael Hadley <michaelwesthadley@gmail.com>
2019-01-13 13:08:26 -0600

    Update app to have main nav menu

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,31 +1,41 @@
 import React, { PureComponent } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { MemoryRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SeedMap from "../genetic-map";
 
-const About = () => <h2>About</h2>;
-const Users = () => <h2>Users</h2>;
+const Nav = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link to="/genetic-play">Genetic Map Play Mode</Link>
+      </li>
+      <li>
+        <Link to="/genetic-record">Genetic Map Record Mode</Link>
+      </li>
+      <li>
+        <Link to="/savers-play">Savers Map Play Mode</Link>
+      </li>
+      <li>
+        <Link to="/savers-record">Savers Map Record Mode</Link>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default class App extends PureComponent {
   render() {
     return (
       <Router>
-        <div>
-          {/* <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about/">About</Link>
-              </li>
-              <li>
-                <Link to="/users/">Users</Link>
-              </li>
-            </ul>
-          </nav> */}
-
-          <Route path="/" render={() => <SeedMap showImage={false} isRecording={true} />} />
-        </div>
+        <Switch>
+          <Route
+            path="/genetic-play"
+            render={() => <SeedMap showImage={true} isRecording={false} />}
+          />
+          <Route
+            path="/genetic-record"
+            render={() => <SeedMap showImage={false} isRecording={true} />}
+          />
+          <Route render={() => <Nav />} />
+        </Switch>
       </Router>
     );
   }
